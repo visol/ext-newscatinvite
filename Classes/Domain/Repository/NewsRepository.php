2ff4cf4032051ea08c9ac48d26e9aa64c381d46b
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2014-09-25 17:35:43 +0200

    [BUGFIX] Override countByDate to consider category invitations (refs #881)

--- Classes/Domain/Repository/NewsRepository.php
+++ Classes/Domain/Repository/NewsRepository.php
@@ -101,4 +101,72 @@ class NewsRepository extends \Tx_News_Domain_Repository_NewsRepository {
 		return $constraint;
 	}
 
+	/**
+	 * Get the count of news records by month/year and
+	 * returns the result compiled as array
+	 *
+	 * @param \Tx_News_Domain_Model_DemandInterface $demand
+	 * @return array
+	 */
+	public function countByDate(\Tx_News_Domain_Model_DemandInterface $demand) {
+		$data = array();
+		$sql = $this->findDemandedRaw($demand);
+
+		// Get the month/year into the result
+		$field = $demand->getDateField();
+		$field = empty($field) ? 'datetime' : $field;
+
+		$where = substr($sql, strpos($sql, 'WHERE '));
+		$join = '';
+
+		$categoryUidPattern = '/tx_newscatinvite_domain_model_invitation.category = \'(\d+)\'/';
+		preg_match($categoryUidPattern, $where, $matches);
+		if ($matches && $categoryUid = $matches[1]) {
+			$join = ' LEFT JOIN (SELECT * FROM tx_newscatinvite_domain_model_invitation WHERE tx_newscatinvite_domain_model_invitation.category = ' . $categoryUid . ' AND tx_newscatinvite_domain_model_invitation.status = 1 GROUP BY tx_newscatinvite_domain_model_invitation.news) tx_newscatinvite_domain_model_invitation ON tx_news_domain_model_news.uid=tx_newscatinvite_domain_model_invitation.news ';
+		}
+
+		$sql = 'SELECT FROM_UNIXTIME(' . $field . ', "%m") AS "_Month",' .
+			' FROM_UNIXTIME(' . $field . ', "%Y") AS "_Year" ,' .
+			' count(FROM_UNIXTIME(' . $field . ', "%m")) as count_month,' .
+			' count(FROM_UNIXTIME(' . $field . ', "%y")) as count_year' .
+			' FROM tx_news_domain_model_news ' .
+			$join .
+			$where;
+
+		if (TYPO3_MODE === 'FE') {
+			$sql .= $GLOBALS['TSFE']->sys_page->enableFields('tx_news_domain_model_news');
+		} else {
+			$sql .= \TYPO3\CMS\Backend\Utility\BackendUtility::BEenableFields('tx_news_domain_model_news') .
+				\TYPO3\CMS\Backend\Utility\BackendUtility::deleteClause('tx_news_domain_model_news');
+		}
+		// strip unwanted order by
+		$sql = $GLOBALS['TYPO3_DB']->stripOrderBy($sql);
+
+		// group by custom month/year fields
+		$orderDirection = strtolower($demand->getOrder());
+		if ($orderDirection !== 'desc' && $orderDirection != 'asc') {
+			$orderDirection = 'asc';
+		}
+		$sql .= ' GROUP BY _Month, _Year ORDER BY _Year ' . $orderDirection . ', _Month ' . $orderDirection;
+		$res = $GLOBALS['TYPO3_DB']->sql_query($sql);
+		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+			$data['single'][$row['_Year']][$row['_Month']] = $row['count_month'];
+		}
+		$GLOBALS['TYPO3_DB']->sql_free_result($res);
+
+		// Add totals
+		if (is_array($data['single'])) {
+			foreach ($data['single'] as $year => $months) {
+				$countOfYear = 0;
+				foreach ($months as $month) {
+					$countOfYear += $month;
+				}
+				$data['total'][$year] = $countOfYear;
+			}
+		}
+
+		return $data;
+	}
+
+
 }
