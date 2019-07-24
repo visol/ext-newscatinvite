30d6700ca0836e5faa16589cb589d82711249246
Andy Meier <andy.meier@visol.ch>
2019-07-24 16:02:55 +0200

    [BUGFIX] get backendgroups from non cached field be_user.usergroup (refs #19520)

--- Classes/Domain/Repository/BackendUserRepository.php
+++ Classes/Domain/Repository/BackendUserRepository.php
@@ -25,11 +25,36 @@ class BackendUserRepository extends \TYPO3\CMS\Extbase\Domain\Repository\Backend
 {
 
     /**
+     * get uncached usergroups of be_user.usergroup for newsinvitations
+     *
      * @param array $usergroups
      *
      * @return array|\TYPO3\CMS\Extbase\Persistence\QueryResultInterface
      */
     public function findByUsergroups($usergroups)
+    {
+        $usergroupConstraints = [];
+        foreach ($usergroups as $usergroup) {
+            $usergroupConstraints[] = 'AND FIND_IN_SET(' . $usergroup . ', usergroup) ';
+        }
+        $statement = 'SELECT * FROM be_users WHERE 1=1 ' . implode($usergroupConstraints) . BackendUtility::BEenableFields('be_users') . BackendUtility::deleteClause('be_users');
+        $query = $this->createQuery();
+        $query->statement($statement);
+
+        return $query->execute(true);
+    }
+
+    /**
+     * get cached usergroups of be_user.usergroup_cached_list
+     *
+     * attention: setCachedList() is only up to date if a user has loged in after groups have been added/removed!
+     * see: web/typo3/sysext/core/Classes/Authentication/BackendUserAuthentication::setCachedList()
+     *
+     * @param array $usergroups
+     *
+     * @return array|\TYPO3\CMS\Extbase\Persistence\QueryResultInterface
+     */
+    public function findByUsergroupsCached($usergroups)
     {
         $usergroupConstraints = [];
         foreach ($usergroups as $usergroup) {
