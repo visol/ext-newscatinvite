0b9237d8b1323f0913731570bb8ff079cb8cf49e
Fabien Udriot <fabien@omic.ch>
2022-08-25 17:52:22 +0200

    [TASK] Rector code refactoring

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Visol\Newscatinvite\Domain\Model\News;
+use GeorgRinger\News\Domain\Model\Category;
+use Visol\Newscatinvite\Domain\Model\BackendUserGroup;
+use Visol\Newscatinvite\Domain\Model\Invitation;
+
+return [
+    News::class => [
+        'tableName' => 'tx_news_domain_model_news',
+        'properties' => [
+            'invitations' => [
+                'fieldName' => 'invitations'
+            ],
+        ]
+    ],
+    Category::class => [
+        'tableName' => 'sys_category',
+    ],
+    BackendUserGroup::class => [
+        'tableName' => 'be_groups',
+    ],
+    Invitation::class => [
+        'tableName' => 'tx_newscatinvite_domain_model_invitation',
+        'properties' => [
+            'creator' => [
+                'fieldName' => 'cruser_id'
+            ],
+        ]
+    ],
+];
