418b8d16cc02a82661e36bab31a949f4bdb47033
Hannes Lau <office@hanneslau.de>
2022-12-02 17:11:27 +0100

    [TASK] Drop default management fields from ext_tables.sql
    They will be readded by the TYPO3 core if required
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/9.3/Feature-85160-AutoCreateManagementDBFieldsFromTCACtrl.html

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,32 +2,11 @@
 # Table structure for table 'tx_newscatinvite_domain_model_invitation'
 #
 CREATE TABLE tx_newscatinvite_domain_model_invitation (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	status int(11) DEFAULT '0' NOT NULL,
 	sent tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	category int(11) unsigned DEFAULT '0' NOT NULL,
 	news int(11) unsigned DEFAULT '0' NOT NULL,
 	approving_beuser int(11) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
- KEY language (l10n_parent,sys_language_uid)
-
 );
 
 #
