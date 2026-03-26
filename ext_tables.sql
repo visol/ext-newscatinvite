609fed2811e1e94ed1cdf6c4806afa1828af2621
Daniel Huf <daniel.huf@visol.ch>
2026-03-26 15:37:58 +0100

    feat: Add index to tx_newscatinvite_domain_model_invitation

--- ext_tables.sql
+++ ext_tables.sql
@@ -7,6 +7,8 @@ CREATE TABLE tx_newscatinvite_domain_model_invitation (
 	category int(11) unsigned DEFAULT '0' NOT NULL,
 	news int(11) unsigned DEFAULT '0' NOT NULL,
 	approving_beuser int(11) unsigned DEFAULT '0' NOT NULL,
+
+	KEY news (news),
 );
 
 #
